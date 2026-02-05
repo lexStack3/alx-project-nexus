c16d9c100e2f1e3dc152ffaff59931025049507f
lexStack3 <alexanderedim80@gmail.com>
2026-02-05 22:24:45 +0100

    Created a base models and a custom 'User' model

---
+++ fastyums/accounts/models.py
@@ -0,0 +1,59 @@
+import uuid
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class BaseModel(models.Model):
+    """
+    An abstract base model to be inherited by other models for common fields:
+    -   created_at
+    -   updated_at
+    """
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def get_absolute_url(self):
+        pass
+
+    class Meta:
+        abstract = True
+        ordering = ['-created_at']
+
+
+class User(AbstractUser, BaseModel):
+    """A model representation of a <User> instance."""
+    class Roles(models.TextChoices):
+        # User roles
+        CUSTOMER = 'CUS', 'Customer'
+        VENDOR = 'VEN', 'Vendor'
+        COURIER = 'COU', 'Courier'
+        ADMIN = 'ADM', 'Admin'
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELD = ['username']
+
+    user_id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    role = models.CharField(
+        max_length=3,
+        choices=Roles.choices,
+        default=ROles.CUSTOMER
+    )
+    phone_number = models.CharField(max_length=15, blank=False, unique-True)
+
+    class Meta:
+        indexes = [
+            models.Index(
+                fields=['email'],
+                name='email_idx'
+            )
+        ]
+        constraints = [
+            models.UniqueConstraints(
+                fields=['email', 'phone_number']
+            )
+        ]
