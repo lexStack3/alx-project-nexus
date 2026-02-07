ae31fc004981bcf179bc81952f22d8261920de36
lexStack3 <alexanderedim80@gmail.com>
2026-02-07 12:29:02 +0100

    Created a viable 'Vendor' model

--- fastyums/vendors/models.py
+++ fastyums/vendors/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+from core.models import BaseModel
+
+
+User = get_user_model()
+
+
+class Vendor(BaseModel):
+    """
+    A model representation of a <Vendor> instance.
+    """
+    owner = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='vendor_profile'
+    )
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        """
+        String representation of a <Vendor> instance.
+        """
+        return self.name
