f457d56dec3164fb4321bcd837bdbdf227a54d11
lexStack3 <alexanderedim80@gmail.com>
2026-02-07 19:13:03 +0100

    Created the Delivery Model

--- fastyums/courier/models.py
+++ fastyums/courier/models.py
@@ -1,3 +1,23 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+from core.models import BaseModel
+from orders.models import Order
+
+
+class Delivery(BaseModel):
+    """
+    A model representation of a <Delivery> instance.
+    """
+    delivery_id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False
+    )
+    order = models.OneToOneField(
+        Order, on_delete=models.CASCADE, related_name='delivery'
+    )
+    
+    def __str__(self):
+        """
+        String representation for a <Delivery> instance.
+        """
+        return f"Delivering of order: {self.order}"
