1b486bcc12546cc19cc1ffaae028373c31e6d945
lexStack3 <alexanderedim80@gmail.com>
2026-02-07 19:27:31 +0100

    Added models to admin site for debugging

--- fastyums/accounts/admin.py
+++ fastyums/accounts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from django.contrib.auth import get_user_model
+from .models import User, Address
 
-User = get_user_model()
 
 admin.site.register(User)
+admin.site.register(Address)
