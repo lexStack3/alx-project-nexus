1c2355f187b65788b13424d6fb53e20965249ba7
lexStack3 <alexanderedim80@gmail.com>
2026-02-08 12:44:42 +0100

    Added more fields for User admin site

--- fastyums/accounts/admin.py
+++ fastyums/accounts/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
+from django.contrib.contenttypes.admin import GenericTabularInline
 from .models import User, Address
 
 
-admin.site.register(User)
+class AddressInline(GenericTabularInline):
+    model = Address
+    extra = 1
+    fields = ['name', 'user', 'street', 'city', 'state', 'is_default']
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = [
+        'user_id', 'email', 'username', 'first_name', 'last_name', 'get_city'
+    ]
+
+    inlines = [AddressInline]
+
+    @admin.display(description='City')
+    def get_city(self, obj):
+        # Fetch teh default address, or the first one available
+        address = obj.addresses.filter(is_default=True).first() or obj.address.first()
+        return address.city if address else '-'
+
+
 admin.site.register(Address)
