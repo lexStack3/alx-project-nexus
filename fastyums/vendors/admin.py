0ce21e7ed36c7bed1b103fffb1510c409e02611c
lexStack3 <alexanderedim80@gmail.com>
2026-02-08 12:45:48 +0100

    Added more fields for the Vendor admin site

--- fastyums/vendors/admin.py
+++ fastyums/vendors/admin.py
@@ -1,2 +1,7 @@
 from django.contrib import admin
 from .models import Vendor
+
+
+@admin.register(Vendor)
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ['vendor_id', 'owner', 'name', 'addresses__city']
