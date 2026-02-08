633910b3ad4ff4cc1c817a5e0b1b723d0e1f0c4e
lexStack3 <alexanderedim80@gmail.com>
2026-02-08 15:58:19 +0100

    Registered django-cors-headers

--- fastyums/fastyums/settings.py
+++ fastyums/fastyums/settings.py
@@ -64,11 +64,15 @@ INSTALLED_APPS = [
 
     # django-restframework
     'rest_framework',
+
+    # django-cors-headers
+    'corsheaders',
 ]
 
 SITE_ID = 1
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -77,6 +81,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:9000",
 ]
 
 ROOT_URLCONF = 'fastyums.urls'
