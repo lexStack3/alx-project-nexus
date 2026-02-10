33bbbc0fa53a71d4c0a57a72ec29125e18a54ee1
lexStack3 <alexanderedim80@gmail.com>
2026-02-10 09:04:13 +0100

    Craeted viewsets for /categories & /products

---
+++ fastyums/api/views/categories.py
@@ -0,0 +1,13 @@
+from rest_framework import viewsets
+from categories.models import Category, Product
+from api.serializers.categories import CategorySerializer, ProductSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
