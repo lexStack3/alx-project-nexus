2e8895da837fd3eb63a4c860458ce32ce256a852
lexStack3 <alexanderedim80@gmail.com>
2026-02-10 09:05:39 +0100

    Created viewset for /orders & /items endpoints

---
+++ fastyums/api/views/orders.py
@@ -0,0 +1,17 @@
+from rest_framework import viewsets
+
+from orders.models import Order, OrderItem
+from api.serializers.orders import (
+    OrderSerializer,
+    OrderItemSerializer
+)
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class OrderItemViewSet(viewsets.ModelViewSet):
+    queryset = OrderItem.objects.all()
+    serializer_class = OrderItemSerializer
