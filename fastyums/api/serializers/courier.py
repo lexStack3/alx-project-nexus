8c47dc97503a21561d12da84733e7400941a4d37
lexStack3 <alexanderedim80@gmail.com>
2026-02-10 08:57:05 +0100

    Created a serializer for the <Delivery> model

---
+++ fastyums/api/serializers/courier.py
@@ -0,0 +1,43 @@
+from rest_framework import serializers
+
+from courier.models import Delivery
+from accounts.models import User
+from orders.models import Order
+
+
+class OrderReceiverSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'user_id', 'full_name', 'phone', 'email'
+        ]
+        read_only = True
+
+
+class CourierUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['user_id', 'username']
+        read_only = True
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    receiver = OrderReceiverSerializer(source='user', read_only=True)
+
+    class Meta:
+        model = Order
+        fields = [
+            'order_id', 'address', 'receiver',
+            'created_at', 'updated_at'
+        ]
+
+class DeliverySerializer(serializers.ModelSerializer):
+    courier = CourierUserSerializer(read_only=True)
+    order = OrderSerializer(read_only=True)
+
+    class Meta:
+        model = Delivery
+        fields = [
+            'delivery_id', 'courier', 'order',
+            'status', 'estimated_delivery_time', 'delivered_at'
+        ]
