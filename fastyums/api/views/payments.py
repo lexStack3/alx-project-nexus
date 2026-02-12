3ed282f59ed869e9044709e1c8bc981609114a84
lexStack3 <alexanderedim80@gmail.com>
2026-02-12 14:08:27 +0100

    Viewset for /payments

--- fastyums/api/views/payments.py
+++ fastyums/api/views/payments.py
@@ -1,8 +1,40 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
+
 from payments.models import Payment
 from api.serializers.payments import PaymentSerializer
+from api.permissions import IsOwner, IsAuthenticatedUser
+from api.filters.payments import PaymentFilter
 
 
 class PaymentViewSet(viewsets.ModelViewSet):
-    queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
+    lookup_field = 'payment_id'
+    filter_class = PaymentFilter
+    search_fields = ['user__username', 'tx_ref']
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.is_staff:
+            return Payment.objects.all()
+
+        if user.role == 'CUS':
+            return Payment.objects.filter(user=user)
+
+        if user.role == 'VEN':
+            return Payment.objects.filter(
+                order__items__product__vendor__owner=user
+            ).distinct()
+
+        return Payment.objects.none()
+
+    def get_permissions(self):
+        if self.request.user and self.request.user.is_staff:
+            return [AllowAny()]
+
+        if self.action in ['list', 'retrieve', 'create']:
+            return [IsAuthenticatedUser()]
+        if self.action in ['update', 'partial_update', 'destroy']:
+            return [IsOwner()]
+        return super().get_permissions()
