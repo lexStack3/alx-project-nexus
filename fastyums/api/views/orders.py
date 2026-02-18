daa7dee7101b42b07a9af7241a5672243bf28faa
lexStack3 <alexanderedim80@gmail.com>
2026-02-18 19:46:35 +0100

    Viewset for /orders endpoint

--- fastyums/api/views/orders.py
+++ fastyums/api/views/orders.py
@@ -47,35 +47,3 @@ class OrderViewSet(viewsets.ModelViewSet):
         if self.action == 'create':
             return OrderCreateSerializer
         return OrderSerializer
-
-    def create(self, validated_data):
-        request = self.context['request']
-        items_data = validated_data.pop('items')
-        address = validated_data.pop('address_is')
-
-        total_price = 0
-
-        order = Order.objects.create(
-            user=request.user,
-            address=address.to_dict,
-            total_price=0
-        )
-
-        for item in items_data:
-            product = Product.objects.get(product_id=item['product'])
-            quantity = item['quantity']
-            price = product.price
-
-            OrderItem.objects.create(
-                order=order,
-                product=product,
-                quantity=quantity,
-                price_at_purchase=price
-            )
-
-            total_price += price * quantity
-
-        order.total_price = total_price
-        order.save()
-
-        return order
